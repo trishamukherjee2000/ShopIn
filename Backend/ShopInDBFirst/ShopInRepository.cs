19b93eb0ba72bd435af473843bf7f54f0a0d2107
trishamukherjee2000 <mukherjeetrisha2000@gmail.com>
2022-12-03 22:00:38 +0530

    2

---
+++ Backend/ShopInDBFirst/ShopInRepository.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopInDBFirstDataAccessLayer.Models;
+
+namespace ShopInDBFirstDataAccessLayer
+{
+    public class ShopInRepository
+    {
+        ShopInDbContext context;
+        public ShopInRepository()
+        {
+            context = new ShopInDbContext();
+
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            List<Category> categoriesList = (from category in context.Categories
+                                             orderby category.CategoryId
+                                             select category).ToList();
+            return categoriesList;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> productList = null;
+            try
+            {
+                productList = context.Products.OrderByDescending(p => p.Rating).ToList();
+            }
+            catch (Exception ex)
+            {
+                productList = null;
+            }
+            return productList;
+        }
+        public Product GetProductDetailsById(String PId)
+        {
+            Product productList = null;
+            try
+            {
+                productList = context.Products.Find(PId);
+            }
+            catch (Exception ex)
+            {
+                productList = null;
+            }
+            return productList;
+        }
+        public List<Product> GetProductsByCategoryId(int CId)
+        {
+            List<Product> productList = null;
+            try
+            {
+                productList = context.Products.Where(p => p.CategoryId == CId).ToList();
+            }
+            catch (Exception ex)
+            {
+                productList = null;
+            }
+            return productList;
+        }
+        public List<Order> GetAllOrdersByUserId(int UId)
+        {
+            List<Order> orderList = null;
+            try
+            {
+                orderList = context.Orders.Where(o => o.UserId == UId).ToList();
+            }
+            catch (Exception ex)
+            {
+                orderList = null;
+            }
+            return orderList;
+        }
+
+
+
+        public bool AddCategory(string categoryName)
+        {
+            bool status = false;
+            Category category = new Category();
+            category.CategoryName = categoryName;
+            try
+            {
+                context.Categories.Add(category);
+                //context.Add<Categories>(category);
+                context.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public bool AddProducts(Product products)
+        {
+            bool status = false;
+            try
+            {
+                products.ProductId = ShopInDbContext.GenerateNewProductId();
+            
+                context.Products.Add(products);
+                context.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public bool AddUsers(User users)
+        {
+            bool status = false;
+            try
+            {
+                context.Users.Add(users);
+                context.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public bool CheckUser(string Email, string Password)
+        {
+            bool status = false;
+            User ob=null;
+            try
+            {
+                ob = context.Users.Where(p => p.UserEmail.Equals(Email) && p.UserPassword.Equals(Password)).FirstOrDefault();
+                if (ob == null)
+                    status = false;
+                else
+                    status = true;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public bool AddOrder(Order ob)
+        {
+            bool status = false;
+            try
+            {
+                context.Orders.Add(ob);
+                context.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+
+                status = false;
+            }
+            return status;
+        }
+
+    }
+}
---
+++ Backend/ShopInDBServices/Controllers/MainController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopInDBFirstDataAccessLayer;
+using ShopInDBFirstDataAccessLayer.Models;
+
+namespace ShopInDBServices.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MainController : Controller
+    {
+        ShopInRepository repository;
+        public MainController()
+        {
+            repository = new ShopInRepository();
+        }
+
+        [HttpGet]
+        public JsonResult GetAllCategories()
+        {
+            List<Category> ob = null;
+            try
+            {
+                ob = repository.GetAllCategories();
+
+            }
+            catch (Exception)
+            {
+                ob = null;
+            }
+            return Json(ob);
+        }
+
+        [HttpGet]
+        public JsonResult GetAllProducts()
+        {
+            List<Product> ob = null;
+            try
+            {
+                ob = repository.GetAllProducts();
+
+            }
+            catch (Exception)
+            {
+                ob = null;
+            }
+            return Json(ob); 
+        }
+
+        [HttpGet]
+        public JsonResult GetProductDetailsById(String PId)
+        {
+            Product ob = null;
+            try
+            {
+                ob = repository.GetProductDetailsById(PId);
+
+            }
+            catch (Exception)
+            {
+                ob = null;
+            }
+            return Json(ob);
+        }
+
+        [HttpGet]
+        public JsonResult GetProductsByCategoryId(int CId)
+        {
+            List<Product> ob = null;
+            try
+            {
+                ob = repository.GetProductsByCategoryId(CId);
+
+            }
+            catch (Exception)
+            {
+                ob = null;
+            }
+            return Json(ob);
+        }
+
+        [HttpGet]
+        public JsonResult GetAllOrdersByUserId(int UId)
+        {
+            List<Order> ob = null;
+            try
+            {
+                ob = repository.GetAllOrdersByUserId(UId);
+
+            }
+            catch (Exception)
+            {
+                ob = null;
+            }
+            return Json(ob);
+        }
+
+        [HttpGet]
+        public JsonResult AddCategory(string categoryName)
+        {
+            bool status = false;
+
+            try
+            {
+                status = repository.AddCategory(categoryName);
+                
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return Json(status);
+        }
+
+        [HttpPost]
+        public JsonResult AddProducts(Product products)
+        {
+            bool status = false;
+            
+
+            try
+            {
+                status = repository.AddProducts(products);
+                
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return Json(status);
+        }
+
+
+        [HttpPost]
+        public JsonResult AddUsers(User users)
+        {
+            bool status = false;
+            string message;
+
+            try
+            {
+                status = repository.AddUsers(users);
+                
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return Json(status);
+        }
+        [HttpPost]
+        public JsonResult AddOrders(Order ob)
+        {
+            bool status = false;
+            try
+            {
+                status = repository.AddOrder(ob);
+            }
+            catch (Exception)
+            {
+
+                status = false;
+            }
+            return Json(status);
+        }
+        [HttpGet]
+        public JsonResult CheckUser(string Email, string Password)
+        {
+            bool ob = false;
+            try
+            {
+                ob = repository.CheckUser(Email,Password);
+
+            }
+            catch (Exception)
+            {
+                ob = false;
+            }
+            return Json(ob);
+        }
+    }
+}
